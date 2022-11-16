a28a9e120bc88168e7d3bcfbe60149c4d3d45c9e
Hendraghz <hendraghz79@gmail.com>
2022-11-16 17:49:26 +0700

    first commit

---
+++ src/component/Login.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Login = () =>{
+
+    return(
+    <div style={{marginTop: "200px"}}>
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-6">
+                    <div className="card p-4">
+                        <div className="card-body">
+                            <h2>Halaman Login</h2>
+                            <div className="form-group">
+                                <label>Username</label>
+                                <input type={"text"} placeholder="Username" className="form-control"></input>
+                            </div>
+                           
+                            <div className="form-group">
+                                <label>Password</label>
+                                <input type={"password"} placeholder="Password" className="form-control">
+                                </input> 
+                            </div>
+
+                            <button className="btn btn-primary">Login</button> 
+                        </div>
+                    </div>
+                </div>    
+            </div>
+        </div>
+    </div>
+    )
+
+}
+
+export default Login;
